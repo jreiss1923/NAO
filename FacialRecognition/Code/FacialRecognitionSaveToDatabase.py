e916de726773eba0310d3e9268e4ca0ca0f15af6
gamerblast <gamerblast@users.noreply.github.com>
2017-12-07 08:27:31 -0500

    Implemented a GUI into the face-saving script

--- FacialRecognition/Code/FacialRecognitionSaveToDatabase.py
+++ FacialRecognition/Code/FacialRecognitionSaveToDatabase.py
@@ -4,35 +4,80 @@ import time
 
 from naoqi import ALProxy
 
-IP = "10.11.25.212"  # Replace here with your NaoQi's IP address.
-PORT = 9559
+from Tkinter import *
 
-# Create a proxy to ALFaceDetection
-try:
-    faceProxy = ALProxy("ALFaceDetection", IP, PORT)
-except Exception, e:
-    print "Error when creating face detection proxy:"
-    print str(e)
-    exit(1)
+IP = '10.31.85.117'
+PORT = 9559
+faceProxy = -1;
+
+def ip_connect():
+    global e
+    global w
+    global IP
+    global faceProxy
+    IP = e.get()
+    
+    print IP
+    
+    # Create a proxy to ALFaceDetection
+    try:
+        faceProxy = ALProxy("ALFaceDetection", IP, PORT)
+    except Exception, ex:
+        print "Error when creating face detection proxy:"
+        print str(ex)
+         #exit(1)
+    #faceProxy.clearDatabase() 
+    e.delete(0, END)
+    w['text'] = "Look directly at the robot and enter your name"
+    b['command'] = learn_face
+        
+def learn_face():
+    global e
+    global w
+    global b
+    global faceProxy
+    name = e.get()
+    
+    print name
+    
+    b.pack_forget()
+    
+    
+    
+    w['text'] = "Detecting face"
+    
+    faceRecognized = faceProxy.learnFace(name)
+
+    if(faceRecognized):
+        print "Face saved"
+        w['text'] = "Face saved"
+    else:
+        w['text'] = "Face not saved, please try again"
+        #exit(1)
+        
+    learnedFaces = faceProxy.getLearnedFacesList()
+
+    for face in learnedFaces:
+        print face
+        
+    #root.quit()
+    
+root = Tk()
+
+w = Label(root, text = "Enter your robot's IP")
+w.pack()
+
+e = Entry(root)
+e.pack()
+e.focus_set()
+
+b = Button(root,text='Next',command=ip_connect)
+b.pack(side='bottom')
+
+root.mainloop()
+
 
 #faceProxy.clearDatabase() # COMMENT THIS OUT AFTER YOU'VE DONE IT ONCE
 
-time.sleep(10)
-
-print "Face saving beginning"
-
 #faceProxy.setRecognitionEnabled(False)
 #faceProxy.setTrackingEnabled(False)
-
-faceRecognized = faceProxy.learnFace("Ohm")
-
-if(faceRecognized):
-    print "Face saved"
-else:
-    print "Face not saved"
-    exit(1)
-
-learnedFaces = faceProxy.getLearnedFacesList()
-
-for face in learnedFaces:
-    print face
